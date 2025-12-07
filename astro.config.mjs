fe22342c25e0ae4d3e87d8fa034cc2f340476797
Nexnaiter <medradotassio@gmail.com>
2025-12-07 18:01:27 -0400

    Correcao final de dependencias e sintaxe Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from 'astro/config';
-import github from '@astrojs/github';
 
 export default defineConfig({
   site: 'https://tassio-code.github.io/portfolio-tassio',
   base: '/portfolio-tassio',
-  adapter: github(),
 });
-
 
